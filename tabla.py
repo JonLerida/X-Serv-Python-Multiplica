84fdad2830a704386b3aa3452da32a7331c6d9f6
JonLerida <joni.leri.96@gmail.com>
2017-01-24 17:15:42 +0100

    Ejercicio1 terminado

--- tabla.py
+++ tabla.py
@@ -1,5 +1,11 @@
-# esto hace lo de las tablas de multi
-#hola prueba x
-for i in range(10):
-  print("y asi")
-fkrkrkrkrkrk
+#Versión 1.0 (para probar) 17:07 24/01
+#
+#
+# Hacer las tablas de multiplicar del 1 al 10
+for i in range(1, 11):
+    # Range(m, n) porque la función va desde m hasta n-1
+    print("Tabla del " + str(i)+"\n-----------")
+    for j in range(1, 11):
+        resultado = i*j
+        print(str(j) + " por " + str(i) + " es " + str(resultado))
+    print("\n")
